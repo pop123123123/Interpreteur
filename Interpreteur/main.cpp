15a218b1e3b7158381a3f7eda869ec459e78faad
mouhatcl <mouhatcl@pc-dg-027-13>
2016-10-05 08:18:40 +0200

    First batch

---
+++ Interpreteur/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: mouhatcl
+ *
+ * Created on 5 octobre 2016, 08:17
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World !";
+    return 0;
+}
+
